965f804dbab653563138438e23df8d5275a09ae0
Mohammad Omar <64026794+anaspui@users.noreply.github.com>
2023-03-07 22:50:50 +0600

    Login Alignment Fixed

--- Home.php
+++ Home.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 
@@ -8,9 +11,9 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
-  
+
   <style>
-     body {
+    body {
       background-color: #000000;
       color: #FFFFFF;
       font-family: 'Roboto Slab', serif;
@@ -54,19 +57,19 @@
     #content p {
       margin-bottom: 20px;
     }
+
     #content img {
-      height: 100vh ;
+      height: 100vh;
       width: 100%;
-    
   </style>
-  
+
 </head>
 
 <body>
-<?php include 'header.php'; ?>
+  <?php include 'header.php'; ?>
   <div id="content">
-   <img src="./img/hq.jpg" alt="">
-</div>
+    <img src="./img/hq.jpg" alt="">
+  </div>
 
 
   <div id="footer">
